e546f9983c239699bcb3d672b79845609c88e6da
Leon <leon.id@qq.com>
2019-05-06 15:12:27 +0800

    fixed;

--- CacheFramework/Program.cs
+++ CacheFramework/Program.cs
@@ -10,11 +10,13 @@ namespace CacheFramework
     {
         static void Main(string[] args)
         {
+            // 注册 CacheModel 类型的缓存，使用 TFunc 约束生产者的方法委托，传入生产者方法的实际参数
             CacheCollectionFactory.RegistCache<CacheModel, Func<int, CacheModel[]>>(
                 CacheCollectionType.List,
                 CacheModelFactory.CreateModels,
                 6);
 
+            // 获取 CacheModel 类型的缓存数据集合，消费者不用考虑缓存何时加载
             var cacheCollection = CacheCollectionFactory.GetCache<CacheModel>().GetCaches();
 
             Console.WriteLine(string.Join("、", cacheCollection.Select(model => model.Name)));
